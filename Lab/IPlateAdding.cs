9f54cccd10bd4dcb95e84bb96381e364c7c551b2
Aleksandar Smukov <aleksandar.smukov@navigator.rs>
2022-07-18 13:24:59 +0200

    -Added IPlateAdding

---
+++ Lab/IPlateAdding.cs
@@ -0,0 +1,14 @@
+﻿using Lab.Models;
+using System.Collections.Generic;
+
+namespace Lab
+{
+    public interface IPlateAdding
+    {
+        List<Well> FillPlate();
+
+        void AddPlate(PlateSizeEnum plateSizeEnum, int id);
+
+        void AddSamplesAndReagents(int numberOfReplicates, List<Sample> samplesToAdd, List<Reagent> reagentsToAdd);
+    }
+}
